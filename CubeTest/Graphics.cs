fbdc87cdbac2dc2d32dea11ebd9c11b441128beb
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-04-01 03:44:02 -0700

    Graphics + UiGraphics: Move UI related code to UiGraphics

--- CubeTest/Graphics.cs
+++ CubeTest/Graphics.cs
@@ -32,24 +32,6 @@ public static unsafe class Graphics {
 	public static TextureFormat SwapchainFormat;
 	public static SwapChain*    Swapchain;
 
-	private static ShaderModule* UiShader;
-
-	public static Texture  UiTexture;
-	public static Sampler* UiSampler;
-
-	private static BindGroupLayout* UiTextureSamplerBindGroupLayout;
-	private static BindGroup*       UiTextureBindGroup;
-
-	private static Buffer* UiProjectionMatrixBuffer;
-
-	private static BindGroupLayout* UiProjectionMatrixBindGroupLayout;
-	private static BindGroup*       UiProjectionMatrixBindGroup;
-
-	private static RenderPipeline* UiPipeline;
-
-	private static ulong   UiVertexBufferSize;
-	private static Buffer* UiVertexBuffer;
-
 	public static void Initialize() {
 		//Register GLFW and SDL windowing/input, for AOT scenarios (like WASM or NativeAOT)
 		GlfwWindowing.RegisterPlatform();
@@ -79,14 +61,13 @@ public static unsafe class Graphics {
 	}
 
 	private static void WindowClosing() {
-		Disposal.Dispose(UiProjectionMatrixBuffer);
-		Disposal.Dispose(UiShader);
+		UiGraphics.Dispose();
 		Disposal.Dispose(Device);
 	}
 
 	private static void FramebufferResize(Vector2D<int> obj) {
 		CreateSwapchain();
-		UpdateUiProjectionMatrixBuffer();
+		UiGraphics.UpdateProjectionMatrixBuffer();
 	}
 
 	private static void Render(double obj) {
@@ -120,12 +101,9 @@ public static unsafe class Graphics {
 			ColorAttachmentCount   = 1,
 			DepthStencilAttachment = null
 		});
-		
-		WebGPU.RenderPassEncoderSetPipeline(renderPass, UiPipeline);
-		WebGPU.RenderPassEncoderSetBindGroup(renderPass, 0, UiTextureBindGroup, 0, null);
-		WebGPU.RenderPassEncoderSetBindGroup(renderPass, 1, UiProjectionMatrixBindGroup, 0, null);
-		WebGPU.RenderPassEncoderSetVertexBuffer(renderPass, 0, UiVertexBuffer, 0, UiVertexBufferSize);
-		WebGPU.RenderPassEncoderDraw(renderPass, 6, 1, 0, 0);
+
+		//Draws a simple textured quad to the screen
+		UiGraphics.TestDraw(renderPass);
 
 		//End the render pass
 		WebGPU.RenderPassEncoderEnd(renderPass);
@@ -209,244 +187,10 @@ public static unsafe class Graphics {
 		//Set up our error callbacks
 		WebGPU.DeviceSetUncapturedErrorCallback(Device, new PfnErrorCallback(UncapturedError), null);
 		WebGPU.DeviceSetDeviceLostCallback(Device, new PfnDeviceLostCallback(DeviceLost), null);
-
-		CreateShaders();
-
+		
 		CreateSwapchain();
 
-		CreateUiProjectionMatrixBuffer();
-		UpdateUiProjectionMatrixBuffer();
-
-		UiTexture = new Texture(ResourceHelpers.ReadResource("Textures/when.png"));
-
-		UiSampler = WebGPU.DeviceCreateSampler(Device, new SamplerDescriptor {
-			AddressModeU = AddressMode.Repeat,
-			AddressModeV = AddressMode.Repeat,
-			AddressModeW = AddressMode.Repeat,
-			MagFilter    = FilterMode.Nearest,
-			MinFilter    = FilterMode.Nearest,
-			MipmapFilter = MipmapFilterMode.Nearest,
-			Compare      = CompareFunction.Undefined,
-		});
-
-		CreateUiProjectionMatrixBindGroup();
-		CreateShaderTextureBindGroup();
-
-		CreatePipeline();
-
-		CreateVertexBuffer();
-	}
-
-	private static void CreateVertexBuffer() {
-		BufferDescriptor descriptor = new BufferDescriptor {
-			Size  = UiVertexBufferSize = (ulong)(sizeof(UiVertex) * 6),
-			Usage = BufferUsage.Vertex | BufferUsage.CopyDst
-		};
-
-		UiVertexBuffer = WebGPU.DeviceCreateBuffer(Device, descriptor);
-
-		UiVertex* data = stackalloc UiVertex[6];
-
-		const float xPos   = 100;
-		const float yPos   = 100;
-		float width  = UiTexture.Size.X * 8;
-		float height = UiTexture.Size.Y * 8;
-
-		//Fill data with a quad with a CCW front face
-		data[0] = new UiVertex(new Vector2(xPos, yPos), new Vector2(0, 0));                        //Top left
-		data[1] = new UiVertex(new Vector2(xPos + width, yPos), new Vector2(1, 0));                //Top right
-		data[2] = new UiVertex(new Vector2(xPos + width, yPos + height), new Vector2(1, 1));       //Bottom right
-		data[3] = new UiVertex(new Vector2(xPos, yPos), new Vector2(0, 0));                        //Top left
-		data[4] = new UiVertex(new Vector2(xPos       + width, yPos + height), new Vector2(1, 1)); //Bottom right
-		data[5] = new UiVertex(new Vector2(xPos, yPos + height), new Vector2(0, 1));               //Bottom left
-
-		//Write the data to the buffer
-		WebGPU.QueueWriteBuffer(Queue, UiVertexBuffer, 0, data, (nuint)UiVertexBufferSize);
-	}
-
-	private static void CreatePipeline() {
-		BlendState blendState = new BlendState {
-			Color = new BlendComponent {
-				SrcFactor = BlendFactor.SrcAlpha,
-				DstFactor = BlendFactor.OneMinusSrcAlpha,
-				Operation = BlendOperation.Add
-			},
-			Alpha = new BlendComponent {
-				SrcFactor = BlendFactor.One,
-				DstFactor = BlendFactor.OneMinusSrcAlpha,
-				Operation = BlendOperation.Add
-			}
-		};
-
-		ColorTargetState colorTargetState = new ColorTargetState {
-			Format    = SwapchainFormat,
-			Blend     = &blendState,
-			WriteMask = ColorWriteMask.All
-		};
-
-		FragmentState fragmentState = new FragmentState {
-			Module      = UiShader,
-			TargetCount = 1,
-			Targets     = &colorTargetState,
-			EntryPoint  = (byte*)SilkMarshal.StringToPtr("fs_main")
-		};
-
-		BindGroupLayout** bindGroupLayouts = stackalloc BindGroupLayout*[2];
-		bindGroupLayouts[0] = UiTextureSamplerBindGroupLayout;
-		bindGroupLayouts[1] = UiProjectionMatrixBindGroupLayout;
-
-		PipelineLayoutDescriptor pipelineLayoutDescriptor = new PipelineLayoutDescriptor {
-			BindGroupLayoutCount = 2,
-			BindGroupLayouts     = bindGroupLayouts
-		};
-
-		PipelineLayout* pipelineLayout = WebGPU.DeviceCreatePipelineLayout(Device, pipelineLayoutDescriptor);
-
-		VertexAttribute* vertexAttributes = stackalloc VertexAttribute[2];
-
-		vertexAttributes[0] = new VertexAttribute {
-			Format         = VertexFormat.Float32x2,
-			Offset         = 0,
-			ShaderLocation = 0
-		};
-		vertexAttributes[1] = new VertexAttribute {
-			Format         = VertexFormat.Float32x2,
-			Offset         = (ulong)sizeof(Vector2),
-			ShaderLocation = 1
-		};
-
-		VertexBufferLayout vertexBufferLayout = new VertexBufferLayout {
-			Attributes     = vertexAttributes,
-			AttributeCount = 2,
-			StepMode       = VertexStepMode.Vertex,
-			ArrayStride    = (ulong)sizeof(UiVertex)
-		};
-
-		RenderPipelineDescriptor renderPipelineDescriptor = new RenderPipelineDescriptor {
-			Vertex = new VertexState {
-				Module      = UiShader,
-				EntryPoint  = (byte*)SilkMarshal.StringToPtr("vs_main"),
-				Buffers     = &vertexBufferLayout,
-				BufferCount = 1
-			},
-			Primitive = new PrimitiveState {
-				Topology         = PrimitiveTopology.TriangleList,
-				StripIndexFormat = IndexFormat.Undefined,
-				FrontFace        = FrontFace.Ccw,
-				CullMode         = CullMode.None
-			},
-			Multisample = new MultisampleState {
-				Count                  = 1,
-				Mask                   = ~0u,
-				AlphaToCoverageEnabled = false
-			},
-			Fragment     = &fragmentState,
-			DepthStencil = null,
-			Layout       = pipelineLayout
-		};
-
-		UiPipeline = WebGPU.DeviceCreateRenderPipeline(Device, renderPipelineDescriptor);
-
-		SilkMarshal.Free((nint)renderPipelineDescriptor.Vertex.EntryPoint);
-		SilkMarshal.Free((nint)fragmentState.EntryPoint);
-
-		Console.WriteLine($"Created pipeline 0x{(nuint)UiPipeline:x}");
-	}
-
-	private static void CreateUiProjectionMatrixBindGroup() {
-		BindGroupLayoutEntry entry = new BindGroupLayoutEntry {
-			Binding = 0,
-			Buffer = new BufferBindingLayout {
-				Type           = BufferBindingType.Uniform,
-				MinBindingSize = (ulong)sizeof(Matrix4x4)
-			},
-			Visibility = ShaderStage.Vertex,
-		};
-
-		UiProjectionMatrixBindGroupLayout = WebGPU.DeviceCreateBindGroupLayout
-		(
-			Device, new BindGroupLayoutDescriptor {
-				Entries    = &entry,
-				EntryCount = 1
-			}
-		);
-
-		BindGroupEntry bindGroupEntry = new BindGroupEntry {
-			Binding = 0,
-			Buffer  = UiProjectionMatrixBuffer,
-			Size    = (ulong)sizeof(Matrix4x4)
-		};
-
-		UiProjectionMatrixBindGroup = WebGPU.DeviceCreateBindGroup
-		(
-			Device, new BindGroupDescriptor {
-				Entries    = &bindGroupEntry,
-				EntryCount = 1,
-				Layout     = UiProjectionMatrixBindGroupLayout
-			}
-		);
-	}
-
-	private static void CreateShaderTextureBindGroup() {
-		BindGroupLayoutEntry* entries = stackalloc BindGroupLayoutEntry[2];
-		entries[0] = new BindGroupLayoutEntry {
-			Binding = 0,
-			Texture = new TextureBindingLayout {
-				Multisampled  = false,
-				SampleType    = TextureSampleType.Float,
-				ViewDimension = TextureViewDimension.TextureViewDimension2D
-			},
-			Visibility = ShaderStage.Fragment
-		};
-		entries[1] = new BindGroupLayoutEntry {
-			Binding = 1,
-			Sampler = new SamplerBindingLayout {
-				Type = SamplerBindingType.Filtering
-			},
-			Visibility = ShaderStage.Fragment
-		};
-
-		BindGroupLayoutDescriptor layoutDescriptor = new BindGroupLayoutDescriptor {
-			Entries    = entries,
-			EntryCount = 2
-		};
-
-		UiTextureSamplerBindGroupLayout = WebGPU.DeviceCreateBindGroupLayout(Device, layoutDescriptor);
-
-		BindGroupEntry* bindGroupEntries = stackalloc BindGroupEntry[2];
-		bindGroupEntries[0] = new BindGroupEntry {
-			Binding     = 0,
-			TextureView = UiTexture.RawTextureView
-		};
-		bindGroupEntries[1] = new BindGroupEntry {
-			Binding = 1,
-			Sampler = UiSampler
-		};
-
-		BindGroupDescriptor descriptor = new BindGroupDescriptor {
-			Entries    = bindGroupEntries,
-			EntryCount = 2,
-			Layout     = UiTextureSamplerBindGroupLayout
-		};
-
-		UiTextureBindGroup = WebGPU.DeviceCreateBindGroup(Device, descriptor);
-	}
-
-	private static void UpdateUiProjectionMatrixBuffer() {
-		//Create our projection matrix
-		Matrix4x4 projectionMatrix = Matrix4x4.CreateOrthographicOffCenter(0, Window.Size.X, Window.Size.Y, 0, 0, 1);
-
-		//Write the projection matrix into the buffer
-		WebGPU.QueueWriteBuffer(Queue, UiProjectionMatrixBuffer, 0, &projectionMatrix, (nuint)sizeof(Matrix4x4));
-	}
-
-	private static void CreateUiProjectionMatrixBuffer() {
-		UiProjectionMatrixBuffer = WebGPU.DeviceCreateBuffer(Device, new BufferDescriptor {
-			Size = (ulong)sizeof(Matrix4x4),
-			//We will be using this buffer as a uniform, and we need CopyDst to write to it using QueueWriteBuffer
-			Usage            = BufferUsage.Uniform | BufferUsage.CopyDst,
-			MappedAtCreation = false
-		});
+		UiGraphics.Initalize();
 	}
 
 	private static void CreateSwapchain() {
@@ -461,24 +205,6 @@ public static unsafe class Graphics {
 		};
 
 		Swapchain = WebGPU.DeviceCreateSwapChain(Device, Surface, swapChainDescriptor);
-	}
-
-	private static void CreateShaders() {
-		byte[] shader = ResourceHelpers.ReadResource("Shaders/Ui.wgsl");
-
-		fixed (byte* ptr = shader) {
-			ShaderModuleWGSLDescriptor wgslDescriptor = new ShaderModuleWGSLDescriptor {
-				Code  = ptr,
-				Chain = new ChainedStruct(sType: SType.ShaderModuleWgsldescriptor)
-			};
-
-			ShaderModuleDescriptor descriptor = new ShaderModuleDescriptor {
-				NextInChain = (ChainedStruct*)(&wgslDescriptor)
-			};
-
-			UiShader = WebGPU.DeviceCreateShaderModule(Device, descriptor);
-			Console.WriteLine($"Shader 0x{(nint)UiShader:x8} created!");
-		}
 	}
 
 	private static void DeviceLost(DeviceLostReason reason, byte* message, void* userData) {
---
+++ CubeTest/UiGraphics.cs
@@ -0,0 +1,317 @@
+using System.Numerics;
+using Silk.NET.Core.Native;
+using Silk.NET.WebGPU;
+using Buffer = Silk.NET.WebGPU.Buffer;
+
+namespace CubeTest;
+
+public static unsafe class UiGraphics {
+	private static ShaderModule* _Shader;
+
+	private static Texture  _Texture = null!;
+	private static Sampler* _Sampler;
+
+	private static BindGroupLayout* _TextureSamplerBindGroupLayout;
+	private static BindGroup*       _TextureBindGroup;
+
+	private static Buffer* _ProjectionMatrixBuffer;
+
+	private static BindGroupLayout* _ProjectionMatrixBindGroupLayout;
+	private static BindGroup*       _ProjectionMatrixBindGroup;
+
+	private static RenderPipeline* _Pipeline;
+
+	private static ulong   _VertexBufferSize;
+	private static Buffer* _VertexBuffer;
+
+	public static void Dispose() {
+		Graphics.Disposal.Dispose(_VertexBuffer);
+		
+		Graphics.Disposal.Dispose(_TextureSamplerBindGroupLayout);
+		Graphics.Disposal.Dispose(_TextureBindGroup);
+		
+		Graphics.Disposal.Dispose(_ProjectionMatrixBindGroupLayout);
+		Graphics.Disposal.Dispose(_ProjectionMatrixBindGroup);
+		
+		Graphics.Disposal.Dispose(_Sampler);
+		_Texture.Dispose();
+		
+		Graphics.Disposal.Dispose(_ProjectionMatrixBuffer);
+		
+		Graphics.Disposal.Dispose(_Pipeline);
+
+		Graphics.Disposal.Dispose(_Shader);
+	}
+
+	public static void Initalize() {
+		CreateShader();
+		
+		CreateProjectionMatrixBuffer();
+		UpdateProjectionMatrixBuffer();
+
+		CreateTestTexture();
+		
+		CreateSampler();
+
+		CreateProjectionMatrixBindGroup();
+		CreateShaderTextureBindGroup();
+
+		CreatePipeline();
+
+		CreateVertexBuffer();
+	}
+
+	private static void CreateVertexBuffer() {
+		BufferDescriptor descriptor = new BufferDescriptor {
+			Size  = _VertexBufferSize = (ulong)(sizeof(UiVertex) * 6),
+			Usage = BufferUsage.Vertex | BufferUsage.CopyDst
+		};
+
+		_VertexBuffer = Graphics.WebGPU.DeviceCreateBuffer(Graphics.Device, descriptor);
+
+		UiVertex* data = stackalloc UiVertex[6];
+
+		const float xPos   = 100;
+		const float yPos   = 100;
+		float       width  = _Texture.Size.X * 8;
+		float       height = _Texture.Size.Y * 8;
+
+		//Fill data with a quad with a CCW front face
+		data[0] = new UiVertex(new Vector2(xPos, yPos), new Vector2(0, 0));                        //Top left
+		data[1] = new UiVertex(new Vector2(xPos + width, yPos), new Vector2(1, 0));                //Top right
+		data[2] = new UiVertex(new Vector2(xPos + width, yPos + height), new Vector2(1, 1));       //Bottom right
+		data[3] = new UiVertex(new Vector2(xPos, yPos), new Vector2(0, 0));                        //Top left
+		data[4] = new UiVertex(new Vector2(xPos       + width, yPos + height), new Vector2(1, 1)); //Bottom right
+		data[5] = new UiVertex(new Vector2(xPos, yPos + height), new Vector2(0, 1));               //Bottom left
+
+		//Write the data to the buffer
+		Graphics.WebGPU.QueueWriteBuffer(Graphics.Queue, _VertexBuffer, 0, data, (nuint)_VertexBufferSize);
+	}
+
+	private static void CreatePipeline() {
+		BlendState blendState = new BlendState {
+			Color = new BlendComponent {
+				SrcFactor = BlendFactor.SrcAlpha,
+				DstFactor = BlendFactor.OneMinusSrcAlpha,
+				Operation = BlendOperation.Add
+			},
+			Alpha = new BlendComponent {
+				SrcFactor = BlendFactor.One,
+				DstFactor = BlendFactor.OneMinusSrcAlpha,
+				Operation = BlendOperation.Add
+			}
+		};
+
+		ColorTargetState colorTargetState = new ColorTargetState {
+			Format    = Graphics.SwapchainFormat,
+			Blend     = &blendState,
+			WriteMask = ColorWriteMask.All
+		};
+
+		FragmentState fragmentState = new FragmentState {
+			Module      = _Shader,
+			TargetCount = 1,
+			Targets     = &colorTargetState,
+			EntryPoint  = (byte*)SilkMarshal.StringToPtr("fs_main")
+		};
+
+		BindGroupLayout** bindGroupLayouts = stackalloc BindGroupLayout*[2];
+		bindGroupLayouts[0] = _TextureSamplerBindGroupLayout;
+		bindGroupLayouts[1] = _ProjectionMatrixBindGroupLayout;
+
+		PipelineLayoutDescriptor pipelineLayoutDescriptor = new PipelineLayoutDescriptor {
+			BindGroupLayoutCount = 2,
+			BindGroupLayouts     = bindGroupLayouts
+		};
+
+		PipelineLayout* pipelineLayout = Graphics.WebGPU.DeviceCreatePipelineLayout(Graphics.Device, pipelineLayoutDescriptor);
+
+		VertexAttribute* vertexAttributes = stackalloc VertexAttribute[2];
+
+		vertexAttributes[0] = new VertexAttribute {
+			Format         = VertexFormat.Float32x2,
+			Offset         = 0,
+			ShaderLocation = 0
+		};
+		vertexAttributes[1] = new VertexAttribute {
+			Format         = VertexFormat.Float32x2,
+			Offset         = (ulong)sizeof(Vector2),
+			ShaderLocation = 1
+		};
+
+		VertexBufferLayout vertexBufferLayout = new VertexBufferLayout {
+			Attributes     = vertexAttributes,
+			AttributeCount = 2,
+			StepMode       = VertexStepMode.Vertex,
+			ArrayStride    = (ulong)sizeof(UiVertex)
+		};
+
+		RenderPipelineDescriptor renderPipelineDescriptor = new RenderPipelineDescriptor {
+			Vertex = new VertexState {
+				Module      = _Shader,
+				EntryPoint  = (byte*)SilkMarshal.StringToPtr("vs_main"),
+				Buffers     = &vertexBufferLayout,
+				BufferCount = 1
+			},
+			Primitive = new PrimitiveState {
+				Topology         = PrimitiveTopology.TriangleList,
+				StripIndexFormat = IndexFormat.Undefined,
+				FrontFace        = FrontFace.Ccw,
+				CullMode         = CullMode.None
+			},
+			Multisample = new MultisampleState {
+				Count                  = 1,
+				Mask                   = ~0u,
+				AlphaToCoverageEnabled = false
+			},
+			Fragment     = &fragmentState,
+			DepthStencil = null,
+			Layout       = pipelineLayout
+		};
+
+		_Pipeline = Graphics.WebGPU.DeviceCreateRenderPipeline(Graphics.Device, renderPipelineDescriptor);
+
+		SilkMarshal.Free((nint)renderPipelineDescriptor.Vertex.EntryPoint);
+		SilkMarshal.Free((nint)fragmentState.EntryPoint);
+
+		Console.WriteLine($"Created pipeline 0x{(nuint)_Pipeline:x}");
+	}
+
+	private static void CreateProjectionMatrixBindGroup() {
+		BindGroupLayoutEntry entry = new BindGroupLayoutEntry {
+			Binding = 0,
+			Buffer = new BufferBindingLayout {
+				Type           = BufferBindingType.Uniform,
+				MinBindingSize = (ulong)sizeof(Matrix4x4)
+			},
+			Visibility = ShaderStage.Vertex,
+		};
+
+		_ProjectionMatrixBindGroupLayout = Graphics.WebGPU.DeviceCreateBindGroupLayout
+		(
+			Graphics.Device, new BindGroupLayoutDescriptor {
+				Entries    = &entry,
+				EntryCount = 1
+			}
+		);
+
+		BindGroupEntry bindGroupEntry = new BindGroupEntry {
+			Binding = 0,
+			Buffer  = _ProjectionMatrixBuffer,
+			Size    = (ulong)sizeof(Matrix4x4)
+		};
+
+		_ProjectionMatrixBindGroup = Graphics.WebGPU.DeviceCreateBindGroup
+		(
+			Graphics.Device, new BindGroupDescriptor {
+				Entries    = &bindGroupEntry,
+				EntryCount = 1,
+				Layout     = _ProjectionMatrixBindGroupLayout
+			}
+		);
+	}
+
+	private static void CreateShaderTextureBindGroup() {
+		BindGroupLayoutEntry* entries = stackalloc BindGroupLayoutEntry[2];
+		entries[0] = new BindGroupLayoutEntry {
+			Binding = 0,
+			Texture = new TextureBindingLayout {
+				Multisampled  = false,
+				SampleType    = TextureSampleType.Float,
+				ViewDimension = TextureViewDimension.TextureViewDimension2D
+			},
+			Visibility = ShaderStage.Fragment
+		};
+		entries[1] = new BindGroupLayoutEntry {
+			Binding = 1,
+			Sampler = new SamplerBindingLayout {
+				Type = SamplerBindingType.Filtering
+			},
+			Visibility = ShaderStage.Fragment
+		};
+
+		BindGroupLayoutDescriptor layoutDescriptor = new BindGroupLayoutDescriptor {
+			Entries    = entries,
+			EntryCount = 2
+		};
+
+		_TextureSamplerBindGroupLayout = Graphics.WebGPU.DeviceCreateBindGroupLayout(Graphics.Device, layoutDescriptor);
+
+		BindGroupEntry* bindGroupEntries = stackalloc BindGroupEntry[2];
+		bindGroupEntries[0] = new BindGroupEntry {
+			Binding     = 0,
+			TextureView = _Texture.RawTextureView
+		};
+		bindGroupEntries[1] = new BindGroupEntry {
+			Binding = 1,
+			Sampler = _Sampler
+		};
+
+		BindGroupDescriptor descriptor = new BindGroupDescriptor {
+			Entries    = bindGroupEntries,
+			EntryCount = 2,
+			Layout     = _TextureSamplerBindGroupLayout
+		};
+
+		_TextureBindGroup = Graphics.WebGPU.DeviceCreateBindGroup(Graphics.Device, descriptor);
+	}
+
+	internal static void UpdateProjectionMatrixBuffer() {
+		//Create our projection matrix
+		Matrix4x4 projectionMatrix = Matrix4x4.CreateOrthographicOffCenter(0, Graphics.Window.Size.X, Graphics.Window.Size.Y, 0, 0, 1);
+
+		//Write the projection matrix into the buffer
+		Graphics.WebGPU.QueueWriteBuffer(Graphics.Queue, _ProjectionMatrixBuffer, 0, &projectionMatrix, (nuint)sizeof(Matrix4x4));
+	}
+
+	private static void CreateProjectionMatrixBuffer() {
+		_ProjectionMatrixBuffer = Graphics.WebGPU.DeviceCreateBuffer(Graphics.Device, new BufferDescriptor {
+			Size = (ulong)sizeof(Matrix4x4),
+			//We will be using this buffer as a uniform, and we need CopyDst to write to it using QueueWriteBuffer
+			Usage            = BufferUsage.Uniform | BufferUsage.CopyDst,
+			MappedAtCreation = false
+		});
+	}
+
+	internal static void TestDraw(RenderPassEncoder* renderPass) {
+		Graphics.WebGPU.RenderPassEncoderSetPipeline(renderPass, _Pipeline);
+		Graphics.WebGPU.RenderPassEncoderSetBindGroup(renderPass, 0, _TextureBindGroup, 0, null);
+		Graphics.WebGPU.RenderPassEncoderSetBindGroup(renderPass, 1, _ProjectionMatrixBindGroup, 0, null);
+		Graphics.WebGPU.RenderPassEncoderSetVertexBuffer(renderPass, 0, _VertexBuffer, 0, _VertexBufferSize);
+		Graphics.WebGPU.RenderPassEncoderDraw(renderPass, 6, 1, 0, 0);
+	}
+
+	private static void CreateSampler() {
+		_Sampler = Graphics.WebGPU.DeviceCreateSampler(Graphics.Device, new SamplerDescriptor {
+			AddressModeU = AddressMode.Repeat,
+			AddressModeV = AddressMode.Repeat,
+			AddressModeW = AddressMode.Repeat,
+			MagFilter    = FilterMode.Nearest,
+			MinFilter    = FilterMode.Nearest,
+			MipmapFilter = MipmapFilterMode.Nearest,
+			Compare      = CompareFunction.Undefined,
+		});
+	}
+
+	private static void CreateTestTexture() {
+		_Texture = new Texture(ResourceHelpers.ReadResource("Textures/when.png"));
+	}
+
+	private static void CreateShader() {
+		byte[] shader = ResourceHelpers.ReadResource("Shaders/Ui.wgsl");
+
+		fixed (byte* ptr = shader) {
+			ShaderModuleWGSLDescriptor wgslDescriptor = new ShaderModuleWGSLDescriptor {
+				Code  = ptr,
+				Chain = new ChainedStruct(sType: SType.ShaderModuleWgsldescriptor)
+			};
+
+			ShaderModuleDescriptor descriptor = new ShaderModuleDescriptor {
+				NextInChain = (ChainedStruct*)(&wgslDescriptor)
+			};
+
+			_Shader = Graphics.WebGPU.DeviceCreateShaderModule(Graphics.Device, descriptor);
+			Console.WriteLine($"Shader 0x{(nint)_Shader:x8} created!");
+		}
+	}
+}
