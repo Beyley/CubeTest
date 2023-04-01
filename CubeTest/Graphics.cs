45de3992a28805b53bd33a817693cbe6cd3df24b
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-03-31 22:25:47 -0700

    Graphics: Create projection matrix

--- CubeTest/Graphics.cs
+++ CubeTest/Graphics.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
 using Silk.NET.Core.Native;
 using Silk.NET.Input.Glfw;
 using Silk.NET.Input.Sdl;
@@ -7,6 +9,7 @@ using Silk.NET.WebGPU.Extensions.Disposal;
 using Silk.NET.Windowing;
 using Silk.NET.Windowing.Glfw;
 using Silk.NET.Windowing.Sdl;
+using Buffer = Silk.NET.WebGPU.Buffer;
 
 namespace CubeTest;
 
@@ -28,6 +31,8 @@ public static unsafe class Graphics {
 
 	private static ShaderModule* UiShader;
 
+	private static Buffer* UiProjectionMatrixBuffer;
+
 	public static void Initialize() {
 		//Register GLFW and SDL windowing/input, for AOT scenarios (like WASM or NativeAOT)
 		GlfwWindowing.RegisterPlatform();
@@ -47,33 +52,23 @@ public static unsafe class Graphics {
 		Window.FramebufferResize += FramebufferResize;
 		Window.Closing           += Dispose;
 	}
-	
+
 	private static void Dispose() {
+		Disposal.Dispose(UiProjectionMatrixBuffer);
 		Disposal.Dispose(UiShader);
 		Disposal.Dispose(Device);
 	}
 
 	private static void FramebufferResize(Vector2D<int> obj) {
 		CreateSwapchain();
+		UpdateUiProjectionMatrixBuffer();
 	}
 
 	private static void Render(double obj) {
-		TextureView* nextView = null;
+		TextureView* nextView = GetNextTextureView();
 
-		for (int attempt = 0; attempt < 2; attempt++) {
-			nextView = WebGPU.SwapChainGetCurrentTextureView(Swapchain);
-
-			if (attempt == 0 && nextView == null) {
-				Console.WriteLine("Getting swapchain TextureView* failed, creating a new swapchain to see if that helps...\n");
-				CreateSwapchain();
-				continue;
-			}
-
-			break;
-		}
-
+		//Lets skip this frame, and try again next frame
 		if (nextView == null) {
-			Console.WriteLine("Failed to get TextureView* for SwapChain* after multiple attempts; giving up.\n");
 			return;
 		}
 
@@ -114,10 +109,34 @@ public static unsafe class Graphics {
 
 		//Submit the command buffer to the queue
 		WebGPU.QueueSubmit(queue, 1, &commandBuffer);
-		
+
 		//Present the swapchain
 		WebGPU.SwapChainPresent(Swapchain);
 		Window.SwapBuffers();
+	}
+
+	[return: MaybeNull]
+	private static TextureView* GetNextTextureView() {
+		TextureView* nextView = null;
+
+		for (int attempt = 0; attempt < 2; attempt++) {
+			nextView = WebGPU.SwapChainGetCurrentTextureView(Swapchain);
+
+			if (attempt == 0 && nextView == null) {
+				Console.WriteLine("Getting swapchain TextureView* failed, creating a new swapchain to see if that helps...\n");
+				CreateSwapchain();
+				continue;
+			}
+
+			break;
+		}
+
+		if (nextView == null) {
+			Console.WriteLine("Failed to get TextureView* for SwapChain* after multiple attempts; giving up.\n");
+			return nextView;
+		}
+
+		return nextView;
 	}
 
 	public static void Load() {
@@ -163,6 +182,38 @@ public static unsafe class Graphics {
 		CreateShaders();
 
 		CreateSwapchain();
+
+		CreateUiProjectionMatrixBuffer();
+		UpdateUiProjectionMatrixBuffer();
+	}
+
+	private static void UpdateUiProjectionMatrixBuffer() {
+		//Get a device queue
+		Queue* queue = WebGPU.DeviceGetQueue(Device);
+
+		//Create our projection matrix
+		Matrix4x4 projectionMatrix = Matrix4x4.CreateOrthographicOffCenter(0, Window.Size.X, Window.Size.Y, 0, 0, 1);
+
+		//Write the projection matrix into the buffer
+		WebGPU.QueueWriteBuffer(queue, UiProjectionMatrixBuffer, 0, &projectionMatrix, (nuint)sizeof(Matrix4x4));
+
+		//Get a command encoder
+		CommandEncoder* commandEncoder = WebGPU.DeviceCreateCommandEncoder(Device, new CommandEncoderDescriptor());
+
+		//Finish the command encoder to get a command buffer
+		CommandBuffer* commandBuffer = WebGPU.CommandEncoderFinish(commandEncoder, new CommandBufferDescriptor());
+
+		//Submit the queue to the GPU
+		WebGPU.QueueSubmit(queue, 1, &commandBuffer);
+	}
+
+	private static void CreateUiProjectionMatrixBuffer() {
+		UiProjectionMatrixBuffer = WebGPU.DeviceCreateBuffer(Device, new BufferDescriptor {
+			Size = (ulong)sizeof(Matrix4x4),
+			//We will be using this buffer as a uniform, and we need CopyDst to write to it using QueueWriteBuffer
+			Usage            = BufferUsage.Uniform | BufferUsage.CopyDst,
+			MappedAtCreation = false
+		});
 	}
 
 	private static void CreateSwapchain() {
