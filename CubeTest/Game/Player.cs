1061463ebe25e5268c7589ba3c99da67f63c7ffb
jvyden <jvyden@jvyden.xyz>
2023-11-22 19:01:58 -0500

    it's kinda movement

--- CubeTest/Game/Player.cs
+++ CubeTest/Game/Player.cs
@@ -16,7 +16,10 @@ public class Player
     public readonly Vector3 Size = new(0.5f, 2f, 0.5f);
 
     public Vector3 Position;
-    public bool OnGround;
+    private Vector3 _velocity;
+    private Vector3 _targetVelocity;
+
+    private bool _onGround;
 
     public float Pitch;
     public float Yaw;
@@ -25,13 +28,26 @@ public class Player
 
     public void Update(float d)
     {
-        _inputHandler.Update(d);
-        if (!OnGround) this.Position.Y -= 2.5f * d;
+        this._inputHandler.Update(d);
+        if (!this._onGround) this._targetVelocity.Y -= 1.0f * d;
 
-        OnGround = IsOnGround();
+        this._onGround = this.IsOnGround();
+
+        if (this._onGround)
+        {
+            this._velocity.Y = 0;
+        }
+
+        if (this._targetVelocity.X != 0) this._velocity.X += Math.Sign(this._targetVelocity.X) * 0.25f * d;
+        else this._velocity.X -= this._velocity.X * 0.25f;
+        
+        if (this._targetVelocity.Z != 0) this._velocity.Z += Math.Sign(this._targetVelocity.Z) * 0.25f * d;
+        else this._velocity.Z -= this._velocity.Z * 0.25f;
+            
+        this.Position += _velocity;
     }
 
-    private bool IsOnGround() => this.Position.Y <= 3.3;
+    private bool IsOnGround() => this.Position.Y <= 0;
     
     public void HandleInputs(float d, FlyInputs inputs)
     {
@@ -39,7 +55,7 @@ public class Player
         Pitch += inputs.Turn.Y;
         Pitch = Math.Clamp(Pitch, -89.99f, 89.99f);
         
-        this.Position += Vector3.Normalize(Vector3.Cross(WorldGraphics.Camera.Front, WorldGraphics.Camera.Up)) * d * inputs.Move.X;
-        this.Position += Vector3.Normalize(WorldGraphics.Camera.Front with { Y = 0 }) * d * inputs.Move.Y;
+        this._targetVelocity = Vector3.Normalize(Vector3.Cross(WorldGraphics.Camera.Front, WorldGraphics.Camera.Up)) * d * inputs.Move.X;
+        this._targetVelocity += Vector3.Normalize(WorldGraphics.Camera.Front with { Y = 0 }) * d * inputs.Move.Y;
     }
 }
